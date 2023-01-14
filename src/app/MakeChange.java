cd1a073abad91195506ea863e2d86bf3599c1220
andre_lacommare <alacommaresb@gmail.com>
2023-01-14 09:19:55 -0800

    figured out coins more

--- src/app/MakeChange.java
+++ src/app/MakeChange.java
@@ -11,10 +11,14 @@ public class MakeChange {
 		double tender = kb.nextDouble();
 		
 		int itemDollarsOnly = (int) itemPrice;
+		
 		double itemCentsOnly = itemPrice - itemDollarsOnly;
+		
 		Coins(itemCentsOnly);
 		
-		double totalReturn = tender - itemPrice;
+		
+		
+		//double totalReturn = tender - itemPrice;
 		//System.out.println(totalReturn);
 		
 		
@@ -27,41 +31,36 @@ public class MakeChange {
 		change = 1.00 - change;
 		change = (change + .005) * 100;
 		
-		if (change % 25 >= 0) {
+		if (change % 25 >= 0 && (int)(change /25) != 0) {
 			int numOfQuarters = (int)(change /25);
 			System.out.println("You are getting " + numOfQuarters + " quarter(s) back.");
 			change = (int)(change %= 25);
-			System.out.println("inside quarters");
-			System.out.println(change);
+			//System.out.println("inside quarters");
+			//System.out.println(change);
 		}
-		if (change % 10 >= 0) {
+		if (change % 10 >= 0 && (int)(change/10) != 0) {
 			int numOfDimes = (int)(change/10);
 			System.out.println("You are getting " + numOfDimes + " dime(s) back.");
 			change = (int)(change %= 10);
-			System.out.println("inside dimes");
-			System.out.println(change);
+			//System.out.println("inside dimes");
+			//System.out.println(change);
 		}
-		if (change % 5 >= 0) {
+		if (change % 5 >= 0 && (int)(change/5) != 0) {
 			int numOfNickels = (int)(change/5);
 			System.out.println("You are getting " + numOfNickels + " nickel(s) back.");
 			change = (int)(change %= 5);
-			System.out.println("inside nickels");
-			System.out.println(change);
+			//System.out.println("inside nickels");
+			//System.out.println(change);
 		}
-		if (change % 1 == 0) {
+		if (change % 1 == 0 && (int)(change/1) != 0) {
 			int numOfPennies = (int)(change/1);
 			System.out.println("You are getting " + numOfPennies + " penny(ies) back.");
-			System.out.println("inside pennies");
-			System.out.println(change);
+			//System.out.println("inside pennies");
+			//System.out.println(change);
 		}
-			
-		
-		System.out.println((int)change);
-		
-		
-		
-		
-		
+		//System.out.println((int)change);
 	}
+	
+	
 
 }
